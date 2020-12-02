0b24e90de375ffb58302a205cc79be5e31efef65
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2020-12-02 00:19:16 -0800

    Add CORS Handling for GetGeo Function (#2)
    * added cors
    
    * added cors test
    
    * bumped dependencies
    
    * bumped node-forge dependencies

--- functions/geocode/geocode.js
+++ functions/geocode/geocode.js
@@ -2,29 +2,35 @@ const functions = require("firebase-functions");
 const { getGoogleCodes } = require("./middleware/google");
 
 exports.getGeo = functions.https.onRequest(async (request, response) => {
-  const address = request.query.address;
+  response.set({ "Access-Control-Allow-Origin": "*" });
+  if (request.method === "OPTIONS") {
+    response.set("Access-Control-Allow-Headers", "authentication");
+    return response.status(200).send("");
+  } else {
+    const address = request.query.address;
 
-  if (Object.keys(request.query).length === 0 || address === "") {
-    return response.status(400).send({
-      error: true,
-      status: 400,
-      message: "Params Error: missing or incorrect address format",
-    });
-  }
-
-  return await getGoogleCodes({ functions, address })
-    .then((res) => {
-      return response.status(res.status).send({
-        error: res.status > 200,
-        status: res.status,
-        message: res.message,
-      });
-    })
-    .catch((err) => {
-      return response.status(500).send({
+    if (Object.keys(request.query).length === 0 || address === "") {
+      return response.status(400).send({
         error: true,
-        status: 500,
-        message: err.message,
+        status: 400,
+        message: "Params Error: missing or incorrect address format",
       });
-    });
+    }
+
+    return await getGoogleCodes({ functions, address })
+      .then((res) => {
+        return response.status(res.status).send({
+          error: res.status > 200,
+          status: res.status,
+          message: res.message,
+        });
+      })
+      .catch((err) => {
+        return response.status(500).send({
+          error: true,
+          status: 500,
+          message: err.message,
+        });
+      });
+  }
 });
